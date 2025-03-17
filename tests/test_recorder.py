1fe35e123c5206c90eaa7c039dad4de9d5c0588a
Sergei Maertens <sergei@maykinmedia.nl>
2025-03-17 21:43:43 +0100

    :sparkles: Implement recording the 'current' version
    Version recording is a heuristic where we grab the version from the
    settings/project (this will be made dynamic later), and combine it with
    the machine name. Only if no similar combination was recorded in the
    past hour do we record it (again).
    
    This 'debounce' mechanism is not meant to be exact, rather just
    account for a containerized landscape where machines can be spun up
    frequently or pods may be re-scheduled and/or restarted due to
    health checks failing.
    
    We *assume* that machines have their clocks properly sychronized via
    NTP to account for (large) clock drift, as we order the deployed
    versions on a timestamp that's generated by each machine/container.
    Some drift (seconds/minutes) should not be a problem either, if
    there are multiple versions recorded close together in time it's
    likely that they'll be the same version due to rolling release
    upgrades. Otherwise, any crashes or odd things showing up will be
    an indication that something is probably wrong.

---
+++ tests/test_recorder.py
@@ -0,0 +1,55 @@
+import logging
+
+import pytest
+
+from django_upgrade_check.models import Version, get_machine_name
+from django_upgrade_check.recorder import record_current_version
+
+
+@pytest.mark.django_db
+def test_record_new_version(settings):
+    settings.RELEASE = "1.2.3"
+    settings.GIT_SHA = "abcd1234"
+    assert not Version.objects.exists()
+
+    record_current_version()
+
+    version = Version.objects.get()
+
+    assert version.version == "1.2.3"
+    assert version.git_sha == "abcd1234"
+    assert version.timestamp is not None
+    assert version.machine_name != ""
+
+
+@pytest.mark.django_db
+def test_record_new_version_debounce(settings, caplog: pytest.LogCaptureFixture):
+    settings.RELEASE = "1.2.3"
+    settings.GIT_SHA = "abcd1234"
+    assert record_current_version() is not None
+
+    # recording again in quick succession should debounce
+    with caplog.at_level(logging.INFO, logger="django_upgrade_check.recorder"):
+        result = record_current_version()
+
+    machine_name = get_machine_name()
+    assert (
+        f"Version 1.2.3 was already recorded on machine {machine_name} in the past "
+        "3600 seconds."
+    ) in caplog.text
+
+    assert result is None
+    assert Version.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_record_new_version_no_debounce_different_version(settings):
+    settings.RELEASE = "1.2.3"
+    settings.GIT_SHA = "abcd1234"
+    assert record_current_version() is not None
+
+    settings.RELEASE = "2.3.0"
+    result = record_current_version()
+
+    assert result is not None
+    assert Version.objects.count() == 2
