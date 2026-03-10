635c60e6197c7cca64034d409978d0e10d354ed8
Sven van de Scheur <svenvandescheur@gmail.com>
2026-03-10 14:09:25 +0100

    :boom: Drop support for Django 4

--- upgrade_check/models.py
+++ upgrade_check/models.py
@@ -1,6 +1,5 @@
 import socket
 
-from django import VERSION
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -58,12 +57,7 @@ class Version(models.Model):
         constraints = [
             models.CheckConstraint(
                 name="non_empty_version",
-                check=~models.Q(version=""),  # Django 4.2 LTS (EOL: 2026-04)
-            )
-            if VERSION < (5, 2)
-            else models.CheckConstraint(
-                name="non_empty_version",
-                condition=~models.Q(version=""),  # Django >= 5.2
+                condition=~models.Q(version=""),
             ),
         ]
         ordering = ("-timestamp",)
